78475e75be1201b538415979c6c7864b3afd64f8
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-07-24 14:58:25 -0300

    Incremento Backend

--- CalculadoraMediaAluno/Form1.cs
+++ CalculadoraMediaAluno/Form1.cs
@@ -16,5 +16,39 @@ namespace CalculadoraMediaAluno
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) //Calcular
+        {
+            float soma, media, valor;
+            soma = 0;
+
+            foreach (Control controle in this.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    valor = Convert.ToSingle(((TextBox)controle).Text);
+                    soma += valor;
+                }
+
+                media = soma / 4;
+                this.Controls["label7"].Text = media.ToString();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Limpar
+        {
+            foreach (Control controle in this.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    ((TextBox)controle).Text = "";
+                }
+
+                this.Controls["label7"].Text = "   ";
+
+
+            }
+        }
     }
 }
